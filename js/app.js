594ca1696bf16fa3075b19fe1d1f1620fad9f164
Andrea Corradi <andreacorradi@hw0345.local>
2019-07-08 11:56:50 +0200

    link to published spreadsheet

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 ;(function(window, undefined) {
 
-	window.APP = {}
-
-	APP.init = function() {
-		APP.dataProcessor = new DataProcessor()
-        APP.tree = new Tree()
-
-        d3.csv("../data/enelx_5levels.csv").then(function(csvData){
-            const data = APP.dataProcessor.process(csvData)
-            console.log("data: ", data)
-            APP.tree.plot(data)
-        })
-	}
+    window.APP = {}
     
+    const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1zu9abUyBMr9NixxCs0yPhX0vcutec1illVoNQIz-rDs/edit?usp=sharing'
+
+    APP.init = function() {
+        APP.dataProcessor = new DataProcessor()
+        APP.tree = new Tree()
+        Tabletop.init({ key: publicSpreadsheetUrl,
+            callback: showInfo,
+            simpleSheet: true
+        })
+    }
+
+    function showInfo(spreadSheetData, tabletop) {
+        //console.log(spreadSheetData)
+        const processedData = APP.dataProcessor.process(spreadSheetData)
+        //console.log("data: ", processedData)
+        APP.tree.plot(processedData)
+    }
+   
     document.addEventListener("DOMContentLoaded", function(e) { 
         console.log('ready')
-		APP.init()
+        APP.init()
     })
 
 })(window)
